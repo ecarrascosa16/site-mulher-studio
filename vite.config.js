556df1ee1f2327c2601bcd42d8b3ee11c517ee3c
Eduardo Carrascosa <eduardoc.codes@gmail.com>
2025-06-24 21:01:51 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
         manifest: true,
         outDir: 'public/build',
         rollupOptions: {
-            input: ['resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
         },
     },
 });
+
